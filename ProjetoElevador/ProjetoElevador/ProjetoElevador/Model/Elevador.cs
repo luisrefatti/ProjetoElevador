e314dc21cd8e6902ee0b2525e075f0452120c61b
Luis Refatti <95254461+luisrefatti@users.noreply.github.com>
2022-03-23 20:59:58 -0300

    Add files via upload

---
+++ ProjetoElevador/ProjetoElevador/ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    internal class Elevador
+    {
+        // ?
+        public int usuario = 1;
+        public int andarTotal = 5;
+        public int andarAtual = 0;
+        public int capacidadeDePessoas = 10;
+        public int totalPessoas = 10;
+
+    }
+}
